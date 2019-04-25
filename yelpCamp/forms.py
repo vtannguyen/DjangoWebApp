60a4604108af92ae664bf132182e81b82e066cfe
tan nguyen <tnguyen@localhost.localdomain>
2019-04-25 07:23:43 +0200

    - A little bit styling for campgrounds page and campgroundsNew page

--- yelpCamp/forms.py
+++ yelpCamp/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
 class NewCampgroundForm(forms.Form):
-    name = forms.CharField(max_length=200)
-    imageUrl = forms.URLField(max_length=400)
+    name = forms.CharField(
+        label='',
+        max_length=200,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}))
+    imageUrl = forms.URLField(
+        label='',
+        max_length=400,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Image Url'}))
