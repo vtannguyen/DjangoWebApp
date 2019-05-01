4d70dfe39b2e44c660099a64ae43c9927b9b64f2
tan nguyen <tnguyen@localhost.localdomain>
2019-05-01 11:27:28 +0200

    - Improve UI   - Change 'description' in Campground model and 'text' in Comment model to more appropriate type

--- yelpCamp/models.py
+++ yelpCamp/models.py
@@ -6,7 +6,7 @@ class Campground(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)
     imageUrl = models.CharField(max_length=500)
-    description = models.CharField(max_length=1000, default='')
+    description = models.TextField(max_length=1000, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Campground(models.Model):
 class Comment(models.Model):
     campground = models.ForeignKey(Campground, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    text = models.CharField(max_length=200)
+    text = models.TextField(max_length=200)
     timestamp = models.DateTimeField('comment timestamp')
 
     def __str__(self):
