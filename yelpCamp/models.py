51595d448bfe018c9f24c9b7234566eaa8b2bc85
tan nguyen <tnguyen@localhost.localdomain>
2019-05-08 14:47:44 +0200

    - Refactor test form and fix small bug (type of imageUrl field in Campground model)

--- yelpCamp/models.py
+++ yelpCamp/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Campground(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)
-    imageUrl = models.CharField(max_length=500)
+    imageUrl = models.URLField(max_length=500)
     description = models.TextField(max_length=1000, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
 
--- yelpCamp/tests/test_forms.py
+++ yelpCamp/tests/test_forms.py
@@ -2,79 +2,102 @@ from django.test import TestCase
 from ..forms import NewCampgroundForm, NewCommentForm, SignUpForm
 
 CORRECT_IMAGE_URL = 'http://www.nextcampsite.com/wp-content/uploads/2014/07/Lost-Creek-Campground-D08.jpg'
-def createUserSignupForm(
-        username='username',
-        email='example@gmail.com',
-        first_name='Michael',
-        last_name='Lee',
-        password1='random123456',
-        password2='random123456',
-        ):
-    form_data = {
-        'username': username,
-        'email': email,
-        'first_name': first_name,
-        'last_name': last_name,
-        'password1': password1,
-        'password2': password2
-    }
-    return SignUpForm(data=form_data)
+
 
 class NewCampgroundFormTest(TestCase):
+    def setUp(self):
+        def createCampgroundForm(
+                name='camp 1',
+                imageUrl=CORRECT_IMAGE_URL,
+                description='This campground is beautiful',
+                price=1.0
+        ):
+            form_data = {'name': name, 'imageUrl': imageUrl, 'description': description, 'price': price}
+            return NewCampgroundForm(data=form_data)
+
+        self.createCampgroundForm = createCampgroundForm
+
+    def tearDown(self):
+        del self.createCampgroundForm
+
     def test_new_campground_form_valid(self):
-        form_data = {
-            'name': 'camp 1',
-            'imageUrl': CORRECT_IMAGE_URL,
-            'description': 'This campground is beautiful',
-            'price': 1.0
-        }
-        form = NewCampgroundForm(data=form_data)
+        form = self.createCampgroundForm()
         self.assertTrue(form.is_valid())
 
     def test_new_campground_form_noname(self):
-        form_data_noname = {'name': '', 'imageUrl': CORRECT_IMAGE_URL}
-        form = NewCampgroundForm(data=form_data_noname)
+        form = self.createCampgroundForm(name=None)
         self.assertFalse(form.is_valid())
 
     def test_new_campground_form_noimage(self):
-        form_data_noimage = {'name': 'camp 1', 'imageUrl': ''}
-        form = NewCampgroundForm(data=form_data_noimage)
+        form = self.createCampgroundForm(imageUrl='')
         self.assertFalse(form.is_valid())
 
     def test_new_campground_form_wrongUrl(self):
-        form_data_wrongUrl = {'name': 'camp 1', 'imageUrl': 'fasdfasdfasdfafds'}
-        form = NewCampgroundForm(data=form_data_wrongUrl)
+        form = self.createCampgroundForm(imageUrl='invalidUrl')
         self.assertFalse(form.is_valid())
 
     def test_new_campground_from_noDescription(self):
-        form_data_noDescription = {'name': 'camp 1', 'imageUrl': CORRECT_IMAGE_URL, 'description': ''}
-        form = NewCampgroundForm(data=form_data_noDescription)
+        form = self.createCampgroundForm(description='')
         self.assertFalse(form.is_valid())
 
+
 class NewCommentFormTest(TestCase):
+    def setUp(self):
+        def createCommentForm(text='this is a comment'):
+            form_data = {'text': text}
+            return NewCommentForm(data=form_data)
+
+        self.createCommentForm = createCommentForm
+
+    def tearDown(self):
+        del self.createCommentForm
+
     def test_new_comment_form_valid(self):
-        form_data = {'text': 'this is a comment'}
-        form = NewCommentForm(data=form_data)
+        form = self.createCommentForm()
         self.assertTrue(form.is_valid())
 
     def test_new_comment_form_invalid(self):
-        form_data = {'text': ''}
-        form = NewCommentForm(data=form_data)
+        form = self.createCommentForm(text='')
         self.assertFalse(form.is_valid())
 
+
 class UserSignupFormTest(TestCase):
+    def setUp(self):
+        def createUserSignupForm(
+                username='username',
+                email='example@gmail.com',
+                first_name='Michael',
+                last_name='Lee',
+                password1='random123456',
+                password2='random123456',
+            ):
+            form_data = {
+                'username': username,
+                'email': email,
+                'first_name': first_name,
+                'last_name': last_name,
+                'password1': password1,
+                'password2': password2
+            }
+            return SignUpForm(data=form_data)
+
+        self.createUserSignupForm = createUserSignupForm
+
+    def tearDown(self):
+        del self.createUserSignupForm
+
     def test_valid_user_signup_form(self):
-        form = createUserSignupForm()
+        form = self.createUserSignupForm()
         self.assertTrue(form.is_valid())
 
     def test_invalid_email_address(self):
-        form = createUserSignupForm(email='notanemail')
+        form = self.createUserSignupForm(email='notanemail')
         self.assertFalse(form.is_valid())
 
     def test_no_first_name(self):
-        form = createUserSignupForm(first_name='')
+        form = self.createUserSignupForm(first_name='')
         self.assertTrue(form.is_valid())
 
     def test_no_last_name(self):
-        form = createUserSignupForm(last_name='')
+        form = self.createUserSignupForm(last_name='')
         self.assertTrue(form.is_valid())
